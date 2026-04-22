f96da8db158d68bd2613b38fdecbee4dbd141bd2
Aleksandr Pilyugin <pilugin.aleksandr@gmail.com>
2026-04-22 15:48:05 +0300

    client was initialized

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './components/Home'
+import Registration from './components/Registration'
+
+function App() {
+
+  return (
+    <div className="bg-sky-200 h-screen">
+        <Home />
+        <Registration />
+    </div>
+  )
+}
+
+export default App
